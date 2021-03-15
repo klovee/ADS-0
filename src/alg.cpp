a4cc2f0cb3108342fdfc71273bb930e8677dd81a
klovee <79109849+klovee@users.noreply.github.com>
2021-03-15 14:02:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,8 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    if (a*b==0) return a+b;
-    if (a<b)return gcd(a, b%a);
-    else return gcd(a%b,b);
+    if (a*b == 0) return a+b;
+    if (a < b)
+        return gcd(a, b%a);
+    else return gcd(a%b, b);
 }
