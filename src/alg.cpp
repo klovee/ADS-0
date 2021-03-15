baf22e1d8a4b7104b7aa16a319dcabe2da2d66cf
klovee <79109849+klovee@users.noreply.github.com>
2021-03-15 13:58:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -2,4 +2,8 @@
 
 int gcd(int a, int b) {
     // поместить сюда текст реализации алгоритма (на С++)
+    if (a*b == 0) return a+b;
+    if (a < b)
+        return gcd(a, b%a);
+    else return gcd(a%b, b);
 }
